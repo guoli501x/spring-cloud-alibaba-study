4f9434e9a76b85561810c353186c0175fb9d6ae7
guoli501x <guoli501x@outlook.com>
2021-02-07 00:19:08 +0800

    spring cloud alibaba nacos 配置中心学习

--- consumer-80/src/main/java/cn/guoli/controller/ConsumerController.java
+++ consumer-80/src/main/java/cn/guoli/controller/ConsumerController.java
@@ -1,5 +1,7 @@
 package cn.guoli.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +15,22 @@ import javax.annotation.Resource;
  * @Author guoli
  * @Since 2021-02-06 2:11
  */
+@RefreshScope
 @RestController
 public class ConsumerController {
+    @Value("${config.info}")
+    private String configInfo;
+
     @Resource
     private RestTemplate restTemplate;
 
     @GetMapping(value = "/echo-rest/{str}")
     public String rest(@PathVariable String str) {
         return restTemplate.getForObject("http://service-provider/echo/" + str, String.class);
+    }
+
+    @GetMapping("/getConfigInfo")
+    public String getConfigInfo() {
+        return configInfo;
     }
 }
